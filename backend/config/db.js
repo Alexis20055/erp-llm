2803e62aa62d1bbcdce523d80381eaaceab01749
Pablo J. <pjeco@uma.es>
2026-05-21 11:17:21 +0200

    Creada la base del backend, pendiente crear modelos, controladores, rutas y el servidor principal

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+//ESsto maneja la conexion con Mongoose
+
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('MongoDB conectado');
+    } catch (error) {
+        console.error('Error de conexión:', error);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
